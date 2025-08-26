64c5fc4a920e515e55f3c2dc3af727c3bd405876
Paaras Dev <161197277+PaarasDev@users.noreply.github.com>
2025-08-27 00:30:05 +0530

    Time: 3 ms (3.01%) | Memory: 15.2 MB (12.09%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,16 +1,54 @@
 class Solution {
 public:
-    void sortColors(vector<int>& nums) {
-        int low = 0, mid = 0, high = nums.size() - 1;
-        
-        while (mid <= high) {
-            if (nums[mid] == 0) {
-                swap(nums[low++], nums[mid++]);
-            } else if (nums[mid] == 1) {
-                mid++;
-            } else {
-                swap(nums[mid], nums[high--]);
-            }
+
+void merge(vector<int>& nums,int l,int mid,int r)
+{
+    vector<int> ans;
+    int left=l;
+    int right=mid+1;
+    while(left<=mid && right<=r)
+    {
+        if(nums[left]<=nums[right])
+        {
+            ans.push_back(nums[left]);
+            left++;
         }
+       else
+        {
+            ans.push_back(nums[right]);
+            right++;
+        }
+    }
+    while(left<=mid)
+    {
+        ans.push_back(nums[left]);
+        left++;
+    }
+    while(right<=r)
+    {
+        ans.push_back(nums[right]);
+        right++;
+    }
+    for(int i=0;i<ans.size();i++)
+    {
+        nums[l+i]=ans[i];
+    }
+}
+void mergeSort(vector<int>& nums,int l,int r)
+{
+    if(l>=r)
+    {
+        return ;
+    }
+    int mid=(l+r)/2;
+    mergeSort(nums,l,mid);
+    mergeSort(nums,mid+1,r);
+    merge(nums,l,mid,r);
+}
+
+    void sortColors(vector<int>& nums) {
+        int l=0;
+        int r=nums.size()-1;
+        mergeSort(nums,l,r);
     }
 };
