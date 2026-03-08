bdae474554c0caf579289bf319b7b73ee341f361
Paaras Dev <161197277+PaarasDev@users.noreply.github.com>
2026-03-09 00:31:12 +0530

    Time: 0 ms (100.00%) | Memory: 13 MB (35.45%) - LeetSync

---
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+
+
+string findDifferentBinaryString(vector<string>& nums) {
+    int n = nums.size();
+    unordered_set<string> numSet(nums.begin(), nums.end());
+    
+    for (int i = 0; i < (1 << n); i++) {
+        string candidate = "";
+        for (int j = 0; j < n; j++) {
+            candidate += ((i & (1 << j)) ? '1' : '0');
+        }
+        
+        if (numSet.find(candidate) == numSet.end()) {
+            return candidate;
+        }
+    }
+    
+    return "";
+    }
+};
